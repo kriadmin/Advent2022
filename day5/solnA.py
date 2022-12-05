0144264ec6ddf2e64c3c6e3b57789acfee513f27
kriadmin <rtanwar2101@gmail.com>
2022-12-05 15:30:34 +0530

    day5 solnA

---
+++ day5/solnA.py
@@ -0,0 +1,25 @@
+import re
+
+with open("input.txt") as f:
+    stacks,moves = f.read().split("\n\n")
+    num_stacks = int(stacks.split("\n")[-1].strip()[-1])
+    boxes = []
+    for line in stacks.split("\n")[:-1]:
+        box = [line[i:i+4].strip() for i in range(0,num_stacks*4,4)]
+        assert len(box) == num_stacks
+        boxes.append(box)
+    boxes = map(lambda x : " ".join(list(reversed(list(x)))).strip().split(" "),zip(*boxes))
+    boxes = list(boxes)
+    print(boxes,"\n\n")
+    for move in moves.split("\n"):
+        srch = re.search("[a-zA-Z ]+([0-9]+)[a-zA-Z ]+([0-9]+)[a-zA-Z ]+([0-9]+)", move)
+        num = int(srch.group(1))
+        from_ = int(srch.group(2))
+        to = int(srch.group(3))
+        for _ in range(num):
+            box = boxes[from_-1].pop()
+            boxes[to-1].append(box)
+    final = []
+    for column in boxes:
+        final.append(column[-1][1])
+    print("".join(final))
